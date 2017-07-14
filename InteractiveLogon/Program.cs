4558033c9a0707418acc6411d65f3e4f3056c983
Mohammed Owaiz Khan <alshell@git>
2017-07-14 18:14:57 +0530

    Add files via upload

---
+++ InteractiveLogon/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+namespace InteractiveLogon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Process = 1st take the values, 2nd export the original reg key (backup) 3rd, feed caption, then text, 4th result.
+            ConsoleSettings(ConsoleColor.Yellow );
+            Console.WriteLine("Customised Interactive logon.- by alshell7");
+            Console.WriteLine("\nMessage title for users attempting to log on :");
+            string caption = Console.ReadLine();
+            Console.WriteLine("\nMessage text for users attempting to log on :");            
+            string text = Console.ReadLine();
+                       
+            string key = "HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System\\ " ;
+            Console.WriteLine("\n\nQuering key " + key +" ...");
+            Console.WriteLine("\nMaking key backup ...");
+            Process.Start("reg", "export " + key + " C:\\reg_logon_backup.reg");
+            Console.WriteLine("\nBackup successfully saved as C:\\reg_logon_backup.reg ...");
+            string arg_cap = key + "/v legalnoticecaption /t REG_SZ /d " + "\""+ caption + "\""+ " /f";
+            string arg_txt = key + "/v legalnoticetext /t REG_SZ /d " + "\"" + text  + "\"" + " /f";            
+            //global::System.Windows.Forms.MessageBox.Show(arg_cap );
+            //global::System.Windows.Forms.MessageBox.Show(arg_txt);
+            Console.WriteLine("\nApplying the settings ...");
+            Process.Start("reg", "add " + arg_cap);
+            Process.Start("reg", "add " + arg_txt);
+            Console.WriteLine("\nSuccessfully Updated !...\n");
+            Console.ReadKey(true);
+        }
+
+        static void ConsoleSettings(ConsoleColor ForegroundColor)
+        {
+            Console.Title = "Customised Interactive logon.";
+            //Console.WindowWidth = 70;
+            Console.BackgroundColor = ConsoleColor.Black  ;
+            Console.ForegroundColor = ForegroundColor  ;            
+        }
+    }
+}
---
+++ InteractiveLogon/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Often used for legal reasons, for example, to warn users about the ramifications of misusing company information or to warn them that their actions may be audited.")]
+[assembly: AssemblyDescription("Often used for legal reasons, for example, to warn users about the ramifications of misusing company information or to warn them that their actions may be audited.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alshell7 Corporation")]
+[assembly: AssemblyProduct("InteractiveLogon")]
+[assembly: AssemblyCopyright("Alshell7 Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7d49b9eb-c322-4110-97b8-d1c992bfb2ea")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
